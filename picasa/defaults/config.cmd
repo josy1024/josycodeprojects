f2359c9caa1889e4eac2210c7c3445de549560fd
josy1024@gmail.com <josy1024@gmail.com@a61783ca-17ed-11de-b6aa-3535bc3264a2>
2011-01-11 18:24:11 +0000

    default import - right way

---
+++ picasa/defaults/config.cmd
@@ -0,0 +1,11 @@
+REM copy this file to same location of the vbs script and change the values you need:
+
+REM examples - change to your directies
+
+REM set basedir="E:\ABLAGE\%date:~6,4%\" 
+set dbshare=\\showme\localvideodisk
+set dbvol=x:
+
+set PICASAPROFILEPATH=%dbvol%\PICASADB
+
+
